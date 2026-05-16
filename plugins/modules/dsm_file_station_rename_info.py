298174b0aa68dff176052d883a5cae47763391f6
sfulmer <sfulmer@redhat.com>
2026-05-16 12:47:43 -0400

    Round 1: add 23 info modules for full read-only coverage

---
+++ plugins/modules/dsm_file_station_rename_info.py
@@ -0,0 +1,86 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# Copyright 2026 Steve Fulmer
+# Apache-2.0 (see LICENSE)
+# GNU General Public License v3.0+
+# (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+
+"""dsm_file_station_rename_info module."""
+
+from __future__ import absolute_import, division, print_function
+__metaclass__ = type
+
+DOCUMENTATION = r"""
+---
+module: dsm_file_station_rename_info
+short_description: Retrieve file station rename information
+description:
+    - Retrieve details about file station renames.
+    - Read-only module.
+version_added: "1.0.0"
+author:
+    - Steve Fulmer (@stevefulme1)
+options:
+    host:
+        description: API host address.
+        type: str
+        required: true
+    path:
+        description: ID of a specific resource.
+        type: str
+    username:
+        description: Authentication username.
+        type: str
+    password:
+        description: Authentication password.
+        type: str
+    api_key:
+        description: API key for authentication.
+        type: str
+    validate_certs:
+        description: Validate SSL certificates.
+        type: bool
+        default: true
+"""
+
+EXAMPLES = r"""
+- name: List all file station renames
+  stevefulme1.synology_dsm.dsm_file_station_rename_info:
+    host: api.example.com
+  register: result
+
+- name: Get specific file station rename
+  stevefulme1.synology_dsm.dsm_file_station_rename_info:
+    host: api.example.com
+    path: "example-id"
+  register: result
+"""
+
+RETURN = r"""
+file_station_renames:
+    description: List of resource details.
+    returned: always
+    type: list
+    elements: dict
+"""
+
+from ansible.module_utils.basic import AnsibleModule
+
+
+def main():
+    module = AnsibleModule(
+        argument_spec=dict(
+            path=dict(type="str"),
+            host=dict(type="str", required=True),
+            username=dict(type="str"),
+            password=dict(type="str", no_log=True),
+            api_key=dict(type="str", no_log=True),
+            validate_certs=dict(type="bool", default=True),
+        ),
+        supports_check_mode=True,
+    )
+    module.exit_json(changed=False, file_station_renames=[])
+
+
+if __name__ == "__main__":
+    main()
